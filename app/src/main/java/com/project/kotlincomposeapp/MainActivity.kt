26279b067509161ffa5ebc4ac50c1703c8c78e28
Alejo Gazagne <gazagnealejo@gmail.com>
2024-09-13 19:07:00 -0300

    Base login

---
+++ app/src/main/java/com/project/kotlincomposeapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.project.kotlincomposeapp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.project.kotlincomposeapp.ui.LoginScreen
+import com.project.kotlincomposeapp.ui.LoginViewModel
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                LoginScreen(LoginViewModel())
+            }
+
+        }
+    }
+}
